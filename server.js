149ec1be79b0bf6608f49acd4fafef247ac49f19
Pramith Prakash <pprakash2@sapient.com>
2018-09-10 15:54:42 +0530

    url fix

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.post('/users', (req, res) => {
   })
   .then(() => res.sendStatus(201))
   .catch(error => {
-    if (error.error_type === '/services/chatkit/user_already_exists') {
+    if (error.error_type === 'services/chatkit/user_already_exists') {
       res.sendStatus(200)
       } else {
         res.status(error.status).json(error)
