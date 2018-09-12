f4f5ba86f04687513fce1e31f030f7d2128cf6df
Pramith Prakash <pprakash2@sapient.com>
2018-09-12 21:06:21 +0530

    heroku fix path get added

--- server.js
+++ server.js
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
+app.use(express.static(__dirname + '/build'));
+
+
 app.post('/users', (req, res) => {
   const { username } = req.body
   chatkit
