17519cfec5558e68e92416894fd5437463143f77
Pramith Prakash <pprakash2@sapient.com>
2018-09-12 13:23:38 +0530

    url fix heruku

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.post('/authenticate', (req, res) => {
   res.status(authData.status).send(authData.body)
 })
 
-const PORT = 3000;
+const PORT =  process.env.PORT || 3001;
 app.listen(PORT, err => {
   if (err) {
     console.error(err)
