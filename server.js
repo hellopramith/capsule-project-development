a220421fd6de6c60ba6173171975eadb359c988b
Pramith Prakash <pprakash2@sapient.com>
2018-09-12 12:38:53 +0530

    rebase unwanted test commits

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ const chatkit = new Chatkit.default({
   key: 'c77f1540-82fa-4d36-9a6e-3c6fa25788e2:CMLQzqBohxdsz2qDexQWeqylSe5toR0GwySWIlBID10=',
 })
 
+const PORT =  process.env.PORT || 3001;
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.post('/users', (req, res) => {
   const { username } = req.body
   chatkit
@@ -23,7 +29,7 @@ app.post('/users', (req, res) => {
   })
   .then(() => res.sendStatus(201))
   .catch(error => {
-    if (error.error_type === 'services/chatkit/user_already_exists') {
+    if (error.error_type === '/services/chatkit/user_already_exists') {
       res.sendStatus(200)
       } else {
         res.status(error.status).json(error)
@@ -36,7 +42,6 @@ app.post('/authenticate', (req, res) => {
   res.status(authData.status).send(authData.body)
 })
 
-const PORT = 3001
 app.listen(PORT, err => {
   if (err) {
     console.error(err)
